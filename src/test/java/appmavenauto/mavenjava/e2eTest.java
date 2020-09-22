6ab0f6ef1015169aec14aaadd26db56f1c14ef53
ryusuf647 <ryusuf647@hotmail.com>
2020-09-22 12:00:03 -0400

    User 1 dev1 team

---
+++ src/test/java/appmavenauto/mavenjava/e2eTest.java
@@ -0,0 +1,21 @@
+package appmavenauto.mavenjava;
+
+import org.testng.annotations.Test;
+
+public class e2eTest {
+	
+	@Test
+	public void firstTest()	{
+		System.out.println("First end 2 end test.");
+	}
+	
+	@Test
+	public void secondTest()	{
+		System.out.println("Second end 2 end test.");
+	}
+	
+	@Test
+	public void fullTest()	{
+		System.out.println("Full end 2 end test.");
+	}
+}
